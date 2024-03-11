c12423c46503e1f3461677915176b0c016f29f84
Palhares17 <mspalhares01@gmail.com>
2024-03-11 09:35:55 -0300

    Update header component to toggle menu visibility

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,44 +3,51 @@ import { InstagramLogo, LinkedinLogo, Briefcase, Rocket, Code, PaperPlaneTilt } 
 import styles from './header.module.css';
 
 function Header() {
-	const [isOpen, setIsOpen] = React.useState(true);
+	const [isOpen, setIsOpen] = React.useState(false);
 
 	const handleOpen = () => {
 		setIsOpen(!isOpen);
 	};
-	
+
 	return (
-		<header className={styles.header}>
-			<div className={styles.nav}>
-				<div className={isOpen ? styles.active : ''}>
-					<div className={styles.buttonNav} onClick={handleOpen}>
-						<span className={styles.menu}></span>
-						<span className={styles.menu}></span>
+		<>
+			{
+				isOpen && <div className={styles.closeModal} onClick={() => setIsOpen(false)}></div>
+			}
+			<header className={styles.header}>
+				<div className={styles.nav}>
+					<div className={isOpen ? styles.active : ''} >
+						<div className={styles.buttonNav} onClick={handleOpen}>
+							<span className={styles.menu}></span>
+							<span className={styles.menu}></span>
+						</div>
 					</div>
+
+					<h2 className={styles.logo}>Matheus</h2>
 				</div>
-
-				<h2 className={styles.logo}>Matheus</h2>
-			</div>
 			
-			{isOpen && (
-				<nav className={styles.modal}>
-					<ul className={styles.items}>
-						<li><Briefcase size={24} className={styles.svg}/>Sobre</li>
-						<li><Rocket size={24} />Projetos</li>
-						<li><Code size={24} />Habilidades</li>
-						<li><PaperPlaneTilt size={24} />Contato</li>
-					</ul>
-				</nav>
-			) }
+				{isOpen && (
+					<>
+						<nav className={styles.modal}>
+							<ul className={styles.items}>
+								<li><Briefcase size={24} className={styles.svg}/>Sobre</li>
+								<li><Rocket size={24} />Projetos</li>
+								<li><Code size={24} />Habilidades</li>
+								<li><PaperPlaneTilt size={24} />Contato</li>
+							</ul>
+						</nav>
+					</>
+				) }
 			
 
-			<div className={styles.containerIcon}>
-				<p className={styles.email}>mspalhares01@gmail.com</p>
-				<span className={styles.divider}></span>
-				<InstagramLogo size={24} color="#585858" />
-				<LinkedinLogo size={24} color="#585858"/>
-			</div>	
-		</header>
+				<div className={styles.containerIcon}>
+					<p className={styles.email}>mspalhares01@gmail.com</p>
+					<span className={styles.divider}></span>
+					<InstagramLogo size={24} color="#585858" />
+					<LinkedinLogo size={24} color="#585858"/>
+				</div>	
+			</header>
+		</>
 	);
 }
 
