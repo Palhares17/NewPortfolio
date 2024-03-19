46a386dbb53064fffd1f8ab99140d9351fac4728
Palhares17 <mspalhares01@gmail.com>
2024-03-19 14:10:57 -0300

    Update ESLint rules and add history component

--- src/about/history/History.jsx
+++ src/about/history/History.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Plus } from '@phosphor-icons/react';
 import styles from './history.module.css';
+import { historyObj } from './history';
 
 function History() {
+	const [width, setWidth] = useState(0);
+
+	useEffect(() => {
+		const handleResize = () => {
+			const widthWindow = window.innerWidth;
+			setWidth(widthWindow);
+		};
+
+		handleResize();
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	const handleMobile = () => {
+		return width < 768;
+	};
+
 	return (
-		<section className={styles.container}>
-			<h4>Curando Ciência da Computação @ UFV</h4>
-			<div className={styles.data}>
-				<p>
-					mar 2020 ~ presente
-				</p>
-				<Plus size={16}/>
-			</div>
-		</section>
+		<div>
+			{historyObj.map(item => (
+				<section className={styles.container} key={item.title}>
+					<h4>{handleMobile() ? item.mobileTitle : item.title}</h4>
+					<div className={styles.data}>
+						<p>{handleMobile() ? item.mobileDate : item.date}</p>
+						<Plus size={16} />
+					</div>
+				</section>
+			))}
+		</div>
 	);
 }
 
---
+++ src/about/history/history.js
@@ -0,0 +1,8 @@
+export const historyObj = [
+	{
+		title: 'Cursando Ciência da Computação @ UFV',
+		date: 'mar 2020 ~ presente',
+		mobileTitle: 'Cursando CDC',
+		mobileDate: 'presente'
+	}
+];
